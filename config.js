a5b81b6e76833e0af07386d3526bbc0a11fe89a5
HoangGymscode <101062283+HoangGymscode@users.noreply.github.com>
2025-06-04 22:31:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js (file này sẽ được tạo tự động trong quá trình deploy)
 window.__ENV = {
-  API_URL: "https://your-real-api.com", // Sẽ bị ghi đè bởi Vercel
+ API_URL: "http://localhost:3000/api", // Sẽ bị ghi đè bởi Vercel
 };
